1a989187937f2030bf52c96d5c79467d1846db1a
sylvain lubwimi <lubwimi@hotmail.com>
2013-11-05 11:18:06 +0100

    Changes title: changes Test to gradegoals.

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
    <head>
-      <title>Test</title>
+      <title>Gradegoals</title>
       <!--<meta charset='utf-8'>-->
       <link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css" />
       <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
